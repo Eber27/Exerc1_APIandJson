c52add7396e9b04fdda5c371c1b83adea6bafa8d
Eber27 <eberfreire@gmail.com>
2021-05-24 21:04:57 -0500

    Exercise1_APIandJson

---
+++ NewQuotes.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace KanyeRestDemo
+{
+    class NewQuotes
+    {
+        public static void kanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"------------------");
+            Console.WriteLine($"kanye:'{kanyeQuote}'");
+            Console.WriteLine($"------------------");
+
+        }
+
+        public static void ronQuote()
+        {
+            var client = new HttpClient();
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"------------------");
+            Console.WriteLine($"Ron:'{ronQuote}'");
+            Console.WriteLine($"------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using KanyeRestDemo;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeandRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewQuotes.kanyeQuote();
+            NewQuotes.ronQuote();
+
+           
+        }  
+    }
+}
